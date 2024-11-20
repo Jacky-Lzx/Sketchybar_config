44c2bc8f796a6672394eec7efab855fa3b5e7123
Zexi. Li <lzx12138@sjtu.edu.cn>
2024-11-20 21:32:13 +0800

    feat: added colors of Catppuccin Mocha.

--- bar.lua
+++ bar.lua
@@ -3,9 +3,9 @@ local colors = require("colors")
 -- Equivalent to the --bar domain
 sbar.bar({
 	-- topmost = "window",
-	height = 32,
-	margin = 4,
-	color = 0x0000000,
+	height = 26,
+	margin = 10,
+	color = colors.Catppuccin.Mocha.Base,
 	y_offset = 4,
 	padding_right = 0,
 	padding_left = 0,
