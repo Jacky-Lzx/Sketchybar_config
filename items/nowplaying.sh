92a59809d95e5559eaa2047d00cfc6e8a9c5ed6e
Zexi. Li <lzx12138@sjtu.edu.cn>
2024-11-22 00:03:03 +0800

    small: small changes.

--- items/nowplaying.sh
+++ items/nowplaying.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-POPUP_SCRIPT="sketchybar -m --set media_ctrl.anchor popup.drawing=toggle"
+# POPUP_SCRIPT="sketchybar -m --set media_ctrl.anchor popup.drawing=toggle"
 
 media_ctrl_anchor=(
   script="$PLUGIN_DIR/media_ctrl.sh"
-  click_script="$POPUP_SCRIPT"
-  popup.horizontal=on
-  popup.align=center
-  popup.height=150
+  label.max_chars=30
+  # click_script="$POPUP_SCRIPT"
+  # popup.horizontal=on
+  # popup.align=center
+  # popup.height=150
 )
 
 sketchybar --add item media_ctrl.anchor center \
