ba20320dfbaebb240b602545b2b9679aa5abab6d
Zexi. Li <lzx12138@sjtu.edu.cn>
2024-11-23 16:53:27 +0800

    feat: updated volume configs.

--- items/widgets/volume.lua
+++ items/widgets/volume.lua
@@ -4,31 +4,12 @@ local settings = require("settings")
 
 local popup_width = 250
 
-local volume_percent = sbar.add("item", "widgets.volume1", {
-	position = "right",
-	icon = { drawing = false },
-	label = {
-		string = "??%",
-		padding_left = -1,
-		font = { family = settings.font.numbers },
-		color = colors.tn_green,
-	},
-})
+local volume_color = colors.Catppuccin.Mocha.Pink
 
-local volume_icon = sbar.add("item", "widgets.volume2", {
+local volume_item = sbar.add("item", "widgets.volume1", {
 	position = "right",
-	padding_right = -1,
 	icon = {
-		string = icons.volume._100,
-		width = 0,
-		align = "left",
-		font = {
-			style = settings.font.style_map["Regular"],
-			size = 14.0,
-		},
-	},
-	label = {
-		color = colors.green,
+		color = volume_color,
 		width = 25,
 		align = "left",
 		font = {
@@ -36,52 +17,48 @@ local volume_icon = sbar.add("item", "widgets.volume2", {
 			size = 14.0,
 		},
 	},
-})
-
-local volume_bracket = sbar.add("bracket", "widgets.volume.bracket", {
-	volume_icon.name,
-	volume_percent.name,
-}, {
-	-- background = { color = colors.tn_black3, border_color = colors.tn_green, border_width = 2 },
+	label = {
+		string = "??%",
+		padding_left = -1,
+		font = { family = settings.font.numbers },
+		color = volume_color,
+	},
+	background = {
+		color = colors.Catppuccin.Mocha.Base
+	},
 	popup = {
 		align = "center",
-		background = { color = colors.tn_black3, border_color = colors.tn_green, border_width = 2 },
+		background = {
+			color = colors.tn_black3,
+			border_color = volume_color,
+			border_width = 2
+		},
 	},
 })
 
--- sbar.add("item", "widgets.volume.padding", {
--- 	position = "right",
--- 	width = settings.group_paddings,
--- })
-
--- add width
-sbar.add("item", { position = "right", width = 6 })
-
 local volume_slider = sbar.add("slider", popup_width, {
-	position = "popup." .. volume_bracket.name,
+	position = "popup." .. volume_item.name,
 	slider = {
-		highlight_color = colors.blue,
+		highlight_color = volume_color,
 		background = {
 			height = 6,
 			corner_radius = 3,
-			color = colors.gray,
+			color = colors.Catppuccin.Mocha.Overlay_0,
 		},
 		knob = {
-			string = "",
+			string = "􀀁",
 			drawing = true,
 		},
 	},
 	background = {
-		color = colors.tn_green,
+		color = volume_color,
 		height = 2,
 		y_offset = -20,
-		border_color = colors.tn_green,
-		border_width = 1,
 	},
 	click_script = 'osascript -e "set volume output volume $PERCENTAGE"',
 })
 
-volume_percent:subscribe("volume_change", function(env)
+volume_item:subscribe("volume_change", function(env)
 	local volume = tonumber(env.INFO)
 	local icon = icons.volume._0
 	if volume > 60 then
@@ -99,17 +76,17 @@ volume_percent:subscribe("volume_change", function(env)
 		lead = "0"
 	end
 
-	volume_icon:set({ label = icon, icon = { color = colors.tn_black3 } })
-	volume_percent:set({ label = lead .. volume .. "%" })
+	-- volume_icon:set({ label = icon, icon = { color = colors.tn_black3 } })
+	volume_item:set({ icon = icon, label = lead .. volume .. "%" })
 	volume_slider:set({ slider = { percentage = volume } })
 end)
 
 local function volume_collapse_details()
-	local drawing = volume_bracket:query().popup.drawing == "on"
+	local drawing = volume_item:query().popup.drawing == "on"
 	if not drawing then
 		return
 	end
-	volume_bracket:set({ popup = { drawing = false } })
+	volume_item:set({ popup = { drawing = false } })
 	sbar.remove("/volume.device\\.*/")
 end
 
@@ -120,9 +97,9 @@ local function volume_toggle_details(env)
 		return
 	end
 
-	local should_draw = volume_bracket:query().popup.drawing == "off"
+	local should_draw = volume_item:query().popup.drawing == "off"
 	if should_draw then
-		volume_bracket:set({ popup = { drawing = true } })
+		volume_item:set({ popup = { drawing = true } })
 		sbar.exec("SwitchAudioSource -t output -c", function(result)
 			current_audio_device = result:sub(1, -2)
 			sbar.exec("SwitchAudioSource -a -t output", function(available)
@@ -131,21 +108,21 @@ local function volume_toggle_details(env)
 				local counter = 0
 
 				for device in string.gmatch(available, "[^\r\n]+") do
-					local color = colors.tn_green
+					local color = colors.Catppuccin.Mocha.Text
 					if current == device then
-						color = colors.tn_cyan
+						color = volume_color
 					end
 					sbar.add("item", "volume.device." .. counter, {
-						position = "popup." .. volume_bracket.name,
+						position = "popup." .. volume_item.name,
 						width = popup_width,
 						align = "center",
 						label = { string = device, color = color },
 						click_script = 'SwitchAudioSource -s "'
 								.. device
 								.. '" && sketchybar --set /volume.device\\.*/ label.color='
-								.. colors.grey
+								.. colors.Catppuccin.Mocha.Text
 								.. " --set $NAME label.color="
-								.. colors.white,
+								.. volume_color,
 					})
 					counter = counter + 1
 				end
@@ -161,8 +138,6 @@ local function volume_scroll(env)
 	sbar.exec('osascript -e "set volume output volume (output volume of (get volume settings) + ' .. delta .. ')"')
 end
 
-volume_icon:subscribe("mouse.clicked", volume_toggle_details)
-volume_icon:subscribe("mouse.scrolled", volume_scroll)
-volume_percent:subscribe("mouse.clicked", volume_toggle_details)
-volume_percent:subscribe("mouse.exited.global", volume_collapse_details)
-volume_percent:subscribe("mouse.scrolled", volume_scroll)
+volume_item:subscribe("mouse.clicked", volume_toggle_details)
+volume_item:subscribe("mouse.exited.global", volume_collapse_details)
+volume_item:subscribe("mouse.scrolled", volume_scroll)
