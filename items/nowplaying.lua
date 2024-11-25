a0729535c1fc58334df6362fc24f6e12711208b2
Zexi. Li <lzx12138@sjtu.edu.cn>
2024-11-25 14:11:02 +0800

    feat: changed nowplaying configs to only show music title.

--- items/nowplaying.lua
+++ items/nowplaying.lua
@@ -4,63 +4,64 @@ local app_icons = require("helpers.icon_map")
 local whitelist = { ["Music"] = true, ["网易云音乐"] = true }
 
 local nowplaying = sbar.add("item", "nowplaying", {
-  position = "right",
-  updates = true,
-  update_freq = 1,
-  icon = {
-    font = "sketchybar-app-font:Regular:14.0",
-    -- string = "􀑪",
-    color = colors.Catppuccin.Mocha.Rosewater,
-    padding_right = 0,
-  },
-  label = {
-    max_chars = 15,
-    align = "center",
-    padding_right = 3,
-    padding_left = 0,
-    color = colors.Catppuccin.Mocha.Rosewater
-  },
-  background = {
-    color = colors.Catppuccin.Mocha.Base
-  }
+	position = "right",
+	updates = true,
+	update_freq = 1,
+	icon = {
+		font = "sketchybar-app-font:Regular:14.0",
+		-- string = "􀑪",
+		color = colors.Catppuccin.Mocha.Rosewater,
+		padding_right = 0,
+	},
+	label = {
+		max_chars = 15,
+		align = "center",
+		padding_right = 3,
+		padding_left = 0,
+		color = colors.Catppuccin.Mocha.Rosewater,
+	},
+	background = {
+		color = colors.Catppuccin.Mocha.Base,
+	},
 })
 
 local function update_music_info(env)
-  local playing_state = env.INFO.state
-  if playing_state == "paused" then
-    nowplaying:set({
-      drawing = false,
-    })
-    return
-  end
+	local playing_state = env.INFO.state
+	if playing_state == "paused" then
+		nowplaying:set({
+			drawing = false,
+		})
+		return
+	end
 
-  local app = env.INFO.app
-  local title = env.INFO.title
-  local artist = env.INFO.artist
+	local app = env.INFO.app
+	local title = env.INFO.title
+	local artist = env.INFO.artist
 
-  local result = "􀑪"
+	local result = "􀑪"
 
-  local lookup = app_icons[app]
-  local icon = ((lookup == nil) and app_icons["Default"] or lookup)
-  if icon == nil then
-    print("No icon for app: " .. app)
-    icon = ""
-  end
+	local lookup = app_icons[app]
+	local icon = ((lookup == nil) and app_icons["Default"] or lookup)
+	if icon == nil then
+		print("No icon for app: " .. app)
+		icon = ""
+	end
 
-  -- Only show info for whitelisted apps
-  if whitelist[app] then
-    result = title .. " - " .. artist
-  end
+	-- Only show info for whitelisted apps
+	if whitelist[app] then
+		-- result = title .. " - " .. artist
+		result = title
+	end
 
-  nowplaying:set({
-    drawing = true,
-    icon = {
-      string = icon
-    },
-    label = {
-      string = result,
-    }
-  })
+	nowplaying:set({
+		drawing = true,
+		icon = {
+			string = icon,
+		},
+		label = {
+			string = result,
+		},
+	})
 end
 
 nowplaying:subscribe({ "media_change" }, update_music_info)
